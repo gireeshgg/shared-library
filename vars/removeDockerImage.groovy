335169260e169d34c51e775873cd5ca6f9d5e518
srinivasbv22 <45447713+srinivasbv22@users.noreply.github.com>
2019-07-07 19:21:07 +0530

    Update removeDockerImage.groovy

--- vars/removeDockerImage.groovy
+++ vars/removeDockerImage.groovy
@@ -1,5 +1,5 @@
 def call(def dockerImageRemove ,def dockerImageName, def userName, def applicationName,def lastSuccessfulBuild){
-  sh "docker rm -f $(docker ps -a)"
-  sh "docker rmi -f $(docker images -aq|sort -u))"
+  //sh "docker rm -f $(docker ps -a)"
+  //sh "docker rmi -f $(docker images -aq|sort -u))"
 }
 
