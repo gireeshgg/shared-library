cab2a81518b5356106d3b397db04096892ead65c
gireeshgg <34162819+gireeshgg@users.noreply.github.com>
2019-07-13 12:35:36 +0530

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,11 +1,13 @@
 def call(def registry, def dockerCredential ,def imageName,def releaseRepo, def snapshotRepo){
     echo "${dockerCredential}"
     
-    docker.withRegistry('https://registry.hub.docker.com', "[dockerCredential]") {
-      
-    //build docker image
+     //build docker image
     image = docker.build("${imageName}:${BUILD_NUMBER}","./PipeSharedLib/Dockerfile")
     
+    //login to docker hub
+    docker.withRegistry('https://registry.hub.docker.com', "${dockerCredential}") {
+      
+   
     //push image to hub
     image.push()
     
