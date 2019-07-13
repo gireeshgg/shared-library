77108e0bbe83a72f40c136a9dac8c63f6ce73166
gireeshgg <34162819+gireeshgg@users.noreply.github.com>
2019-07-13 13:47:16 +0530

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -2,7 +2,7 @@ def call(def registry, def dockerCredential ,def imageName,def releaseRepo, def 
     echo "${dockerCredential}"
     sh 'pwd'
      //build docker image
-    image = docker.build("${imageName}:${BUILD_NUMBER}","Dockerfile")
+    image = docker.build("${imageName}:${BUILD_NUMBER}",".")
     
     //login to docker hub
     docker.withRegistry('https://registry.hub.docker.com', "${dockerCredential}") {
