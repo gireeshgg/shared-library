597993db954cdfcafcac297318e920a0d039ddf9
gireeshgg <34162819+gireeshgg@users.noreply.github.com>
2019-07-15 15:12:12 +0530

    Create deployToTest.groovy

---
+++ vars/deployToTest.groovy
@@ -0,0 +1,5 @@
+ def call(def deployTo){
+      sh "scp docker-compose.yaml  devopsinfra@${deployTo}:./"
+	    sh "scp nginx.conf devopsinfra@${deployTo}:./"
+	    sh "ssh devopsinfra@${deployTo} 'sudo docker-compose up -d' "
+}
