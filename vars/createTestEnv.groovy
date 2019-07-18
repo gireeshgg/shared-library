a2ebce91f8604e4891868bb0592615d9608a6471
gireeshgg <34162819+gireeshgg@users.noreply.github.com>
2019-07-18 16:29:19 +0530

    Update createTestEnv.groovy

--- vars/createTestEnv.groovy
+++ vars/createTestEnv.groovy
@@ -1,7 +1,7 @@
 def call(){
 
         sh "cd terraform && terraform apply -auto-approve"
-        sh "export TERRAFORMVM=`cat public_ip.txt`"
+        sh "cd terraform && export TERRAFORMVM=`cat public_ip.txt`"
         sh "sudo sh replaceip.sh"
         sh "sudo ansible-playbook ../PipeSharedLib/ansibleFiles/installpackages.yml -vv "
   
